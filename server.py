dafa2921b627167738b9d6a88c16000d83f40e46
nicoechaniz <nicoechaniz@altermundi.net>
2013-10-17 20:49:40 +0200

    added app_factory

--- server.py
+++ server.py
@@ -4,44 +4,49 @@ import os
 
 from flask import Flask, redirect, url_for
 
-from database import db
+def create_app(config_object="settings.DevelopmentConfig"):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
 
-app = Flask(__name__)
-app.config.from_object("settings.DevelopmentConfig")
-db.init_app(app)
+    from database import db
+    db.init_app(app)
 
-from filters.nl2br import nl2br
-app.jinja_env.filters['nl2br'] = nl2br
+    from filters.nl2br import nl2br
+    app.jinja_env.filters['nl2br'] = nl2br
 
-from blueprints.experiment import bpExperiment
-app.register_blueprint(bpExperiment, url_prefix="/experiment")
+    from blueprints.experiment import bpExperiment
+    app.register_blueprint(bpExperiment, url_prefix="/experiment")
 
-from blueprints.node import bpNode
-app.register_blueprint(bpNode, url_prefix="/node")
+    from blueprints.node import bpNode
+    app.register_blueprint(bpNode, url_prefix="/node")
 
-from blueprints.command import bpCommand
-app.register_blueprint(bpCommand, url_prefix="/command")
+    from blueprints.command import bpCommand
+    app.register_blueprint(bpCommand, url_prefix="/command")
 
-from blueprints.nodeapi import bpNodeAPI
-app.register_blueprint(bpNodeAPI, url_prefix="/api")
+    from blueprints.nodeapi import bpNodeAPI
+    app.register_blueprint(bpNodeAPI, url_prefix="/api")
 
-from blueprints.admin import bpAdmin
-app.register_blueprint(bpAdmin, url_prefix="/admin")
+    from blueprints.admin import bpAdmin
+    app.register_blueprint(bpAdmin, url_prefix="/admin")
 
-from blueprints.firmware import bpFirmware
-app.register_blueprint(bpFirmware, url_prefix="/firmware")
+    from blueprints.firmware import bpFirmware
+    app.register_blueprint(bpFirmware, url_prefix="/firmware")
 
-@app.before_first_request
-def initializeFolders():
-    if not os.path.isdir(app.config["OVERLAY_DIR"]):
-        os.makedirs(app.config["OVERLAY_DIR"])
+    @app.before_first_request
+    def initializeFolders():
+        if not os.path.isdir(app.config["OVERLAY_DIR"]):
+            os.makedirs(app.config["OVERLAY_DIR"])
 
-    if not os.path.isdir(app.config["FIRMWARE_DIR"]):
-        os.makedirs(app.config["FIRMWARE_DIR"])
+        if not os.path.isdir(app.config["FIRMWARE_DIR"]):
+            os.makedirs(app.config["FIRMWARE_DIR"])
 
-@app.route("/")
-def index():
-    return redirect(url_for("experiment.list"))
+    @app.route("/")
+    def index():
+        return redirect(url_for("experiment.list"))
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0")

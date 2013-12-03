74adf4fddfb0ef1b8c8ba00e5ad1db09d2de4bd9
Manos Dimogerontakis <emm.dimogerontakis@gmail.com>
2013-12-03 19:07:44 +0100

    Enable autoreload in tornado server

--- tornadoserver.py
+++ tornadoserver.py
@@ -1,8 +1,12 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado import autoreload
 from server import create_app
 
+
 http_server = HTTPServer(WSGIContainer(create_app("settings.DevelopmentConfig")))
-http_server.listen(80)
-IOLoop.instance().start()
+http_server.listen(5000)
+ioloop = IOLoop.instance()
+autoreload.start(ioloop)
+ioloop.start()

752fccfe25f5f9c1f15ce0ecb89403736c91323c
Manos <emm.dimogerontakis@gmail.com>
2014-03-03 18:40:13 +0100

    Implement feature #380 deleting the finished admin commands

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,10 +1,12 @@
 """ Admin-related functionality. """
 
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from models.node import Node
 from models.command import Command
+
+from database import db
 
 from restrictions import get_nodes
 
@@ -16,3 +18,16 @@ def index():
 
     return render_template("admin/index.html", commands=commands,\
 			nodes=get_nodes("Node.available == True"))
+
+
+@bpAdmin.route("/delCom")
+def delCom():
+	for command in Command.query.filter(Command.experimentId == None).all() :
+		if command.executions.count() == command.nodes.count():
+			for execution in command.executions.all():
+		    		db.session.delete(execution)
+	    		db.session.delete(command)
+    	db.session.commit()
+    	return redirect(url_for(".index"))
+
+

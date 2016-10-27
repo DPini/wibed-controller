b981b55e6e932ea6c3c047890a4b6442b3e3b33d
David Pinilla Caparrós <dpinitux@gmail.com>
2016-10-27 15:37:01 +0200

    Bugfix: Crash when non-ascii chars in commands out There was a print in a jinja2 filter, that was causing a crash when trying to convert the command output to ascii.
    It can be solved converting the string to ascii and ignoring the errors,
    but I don't think that this print is used anymore for debugging purposes

--- filters/nl2br.py
+++ filters/nl2br.py
@@ -6,7 +6,6 @@ _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    print(value)
     result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', Markup('<br />\n')) \
         for p in _paragraph_re.split(escape(value)))
     if eval_ctx.autoescape:
